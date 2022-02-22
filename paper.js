4b182aae963184efe433d2041385480ee0a88498
A-glitch-sudo <76194488+A-glitch-sudo@users.noreply.github.com>
2022-02-22 18:35:12 +1300

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,25 @@
+class Paper{
+    constructor(x,y,r){
+      var options={
+          isStatic: false,
+          restitition: 0.3,
+          friction: 0,
+          density: 1.2
+      }  
+      this.x=x;
+      this.y=y;
+      this.r=r;
+      this.image = loadImage("paper.png");
+      this.body = Bodies.circle(this.x,this.y,this.r,);
+      World.add(world,this.body)
+    }
+    display(){
+      var Pos = this.body.position;
+      
+      push()
+     translate(Pos.x,Pos.y);
+     imageMode(CENTER)
+     image(this.image,0,0,this.r,this.r)
+     pop()
+    }
+}
